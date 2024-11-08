45f544a85b33ee2aaf8ab5ff525ec1e7cad023cb
lao.water <baciu.raul555@gmail.com>
2024-11-08 17:53:18 +0200

    Remove submodule and re-add food_prophet as a regular directory

---
+++ node_backend/food_prophet/src/App.js
@@ -0,0 +1,23 @@
+// src/App.js
+
+import React from 'react';
+import StomachFullnessGraph from './Components/main_graph';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  max-width: 900px;
+  width: 100%;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <h1>Welcome to the Stomach Fullness Tracker!</h1>
+      <StomachFullnessGraph />
+    </AppContainer>
+  );
+}
+
+export default App;
