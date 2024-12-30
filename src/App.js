a269f832d03a0c383902d3be28281a99a3027cac
lao.water <baciu.raul555@gmail.com>
2024-12-30 04:44:51 +0200

    integrating react app in django

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import styled from 'styled-components';
 import {
   Header,
   Logo,
@@ -22,41 +21,7 @@ import AddArchetypeModal from './Components/AddArchetypeModal';
 import DigestiveHealth from './Components/DigestiveHealth'; // Import the new component
 import QuoteComponent from './Components/QuoteComponent';
 import { archetypeConfig } from './Components/ArchetypeSimulator';
-
-
-// Styled components
-const AppContainer = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  padding: 0px;
-  text-align: center;
-  position: relative; /* Required for pseudo-elements */
-  overflow: hidden;
-
-  /* Background handled by pseudo-element */
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-image: 
-      linear-gradient(to bottom, rgba(51, 51, 51, 1), rgba(51, 51, 51, 0.7), rgba(0, 0, 0, 0.7)), /* Dark gradient blending */
-      url("/white_lotus.jpg");
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed; /* Parallax effect */
-    filter: brightness(1) blur(0px); /* Optional brightness and blur */
-    z-index: -1; /* Ensures it's behind all content */
-  }
-`;
-
-
-const ContentWrapper = styled.div`
-  margin-top: 70px; /* Add margin to compensate for the fixed header */
-  text-align: center; /* Ensures children like buttons are centered */
-`;
+import './styles/App.css';
 
 
 function App() {
@@ -79,8 +44,8 @@ function App() {
 
   return (
     <Router>
-      <AppContainer>
-        {/* Quote Component */}
+    <div className="AppContainer">
+      {/* Quote Component */}
         <QuoteComponent />
         {/* Header with Navigation Links */}
         <Header>
@@ -92,7 +57,7 @@ function App() {
         </Header>
 
         {/* Main Content */}
-        <ContentWrapper>
+        <div className="ContentWrapper">
           <Routes>
             <Route
               path="/"
@@ -132,8 +97,8 @@ function App() {
               element={<DigestiveHealth />} // "Coming Soon" Page
             />
           </Routes>
-        </ContentWrapper>
-      </AppContainer>
+        </div>
+      </div>
     </Router>
   );
 }
